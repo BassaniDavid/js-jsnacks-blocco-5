88d03e3de181d9de2c6836e242cda73ae4ea3704
BassaniDavid <davbassan98@gmail.com>
2025-03-12 14:25:27 +0100

    feat: snack6 first array

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,6 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const plu15Array =zucchine.filter((zucchina) => zucchina.length > 15);
+console.log(plu15Array);
