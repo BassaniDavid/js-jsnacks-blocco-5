04af76281d03998c8a57ba5ad81ba1cc642143b8
BassaniDavid <davbassan98@gmail.com>
2025-03-12 13:58:33 +0100

    feat: snack1 complete

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// metodo con foreach
+names.forEach((element) => {console.log(element)});
+
